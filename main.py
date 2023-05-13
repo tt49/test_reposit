cac49aeae6ff3424593aa24c112b6f7e9a2ad3fa
Кожушко Максим (Maxerton) <maxerton13.k@gmail.com>
2023-05-13 14:31:42 +0300

    comm

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+
 class Human:
-    age = 12
-    school = 101
-print(Human.age, Human.school)
+    def __init__(self):
+        self.life= True
+        self.progress=1
+        self.rad = 0
+    def sleep(self):
+        print("GO SLEEP")
+        self.rad+=1
+    def study(self):
+        print("GO STUDY")
+        self.progress+=1
+        self.rad-=0.5
+    def chill(self):
+        self.rad+=1
+        self.progress-=0.5
+    def check(self):
+        if(self.progress<=0):
+            print("DEGROD DIE")
+        elif (self.rad<=0):
+            print("DEPRESS DIE")
+    def dayover(self):
+        print("Your happiness level is {0}\n Your progess level is {1}".format(self.rad, self.progress))
