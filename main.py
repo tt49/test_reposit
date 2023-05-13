4549af63a9ef05a6d7bc7c7cb7ed4101fde113aa
Кожушко Максим (Maxerton) <maxerton13.k@gmail.com>
2023-05-13 14:51:28 +0300

    comm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import random
 class Human:
     def __init__(self):
         self.life= True
@@ -17,7 +17,25 @@ class Human:
     def check(self):
         if(self.progress<=0):
             print("DEGROD DIE")
+            self.life = False
         elif (self.rad<=0):
             print("DEPRESS DIE")
+            self.life = False
     def dayover(self):
         print("Your happiness level is {0}\n Your progess level is {1}".format(self.rad, self.progress))
+    def simulate(self):
+        rnd=random.randint(1,3)
+        if (rnd==1):
+            self.study()
+        elif(rnd==2):
+            self.chill()
+        else:
+            self.chill()
+        self.check()
+        self.dayover()
+
+for i in range(10):
+
+    Human().simulate()
+    if(Human().life==False):
+        break
